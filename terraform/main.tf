e525028ba9a7eee052a9e053ced7f5cba46919c5
sujatadhl <dahalsujata52@gmail.com>
2024-06-02 21:05:24 +0545

    Addes workflow file

--- terraform/main.tf
+++ terraform/main.tf
@@ -16,7 +16,24 @@ module "ec2" {
   tags = {
     Name = each.value
   }
-   user_data = file("userdata.sh")
+    user_data=base64encode(<<-EOF
+  #!/bin/bash
+    sudo yum update -y
+
+    sudo amazon-linux-extras install -y python3.8
+
+    sudo python3.8 -m ensurepip
+    sudo python3.8 -m pip install --upgrade pip
+    sudo yum install git 
+
+    sudo python3.8 -m pip install ansible
+    python3.8 -m pip install botocore
+    python3.8 -m pip install boto3
+
+    ansible-pull -U https://github.com/sujatadhl/pipeline-github.git -i aws_ec2.yml -C github-actions ansible/playbook.yml
+
+    EOF
+    )
 }
 
 # #S3 Bucket
