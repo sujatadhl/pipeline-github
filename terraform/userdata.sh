79b32403c3bd1f613277f951fdd8c476857f7e14
sujatadhl <dahalsujata52@gmail.com>
2024-06-04 11:20:01 +0545

    Modified workflow

--- terraform/userdata.sh
+++ terraform/userdata.sh
@@ -9,7 +9,7 @@ sudo python3.8 -m pip install --upgrade pip
 sudo python3.8 -m pip install ansible
 sudo amazon-linux-extras install -y awscli
 
-aws s3 cp s3://sujata-linux2/ansible /home/ansible --recursive
+aws s3 cp s3://sujata-linux2 /home/ansible --recursive
 
 cd /home/ansible
 ansible-playbook -i aws_ec2.yml playbook.yml
